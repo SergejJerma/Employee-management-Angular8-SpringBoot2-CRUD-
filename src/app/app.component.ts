ede1e0cf4d24b150cac54964e074989c9bd332dc
Serzas <j_sergej@yahoo.com>
2019-12-16 17:23:19 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employee-system-client';
+  title = 'Employee system (Angular8 + Spring Boot2)';
 }
